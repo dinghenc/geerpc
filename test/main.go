dfcfd0a6f2dc1ad1de0dcacc2ed4ab1c060a9524
dinghe <308024784@qq.com>
2021-04-26 17:31:01 +0800

    feat - support http protocol and debug

--- test/main.go
+++ test/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"geerpc"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -20,27 +22,28 @@ func (f Foo) Sum(args Args, reply *int) error {
 }
 
 func startServer(addr chan string) {
+	l, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		log.Fatalf("network error: %v", err)
+	}
+
 	var foo Foo
 	if err := geerpc.Register(&foo); err != nil {
 		log.Fatalf("register error: %v", err)
 	}
 
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatalf("network error: %v", err)
-	}
+	geerpc.HandleHTTP()
+
 	log.Printf("start rpc server on %v", l.Addr())
 	addr <- l.Addr().String()
-	geerpc.Accept(l)
+	_ = http.Serve(l, nil)
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-
-	client, _ := geerpc.Dial("tcp", <-addr)
-	defer func() { _ = client.Close() }()
+func call(addrCh chan string) {
+	client, _ := geerpc.DialHTTP("tcp", <-addrCh)
+	defer func() {
+		_ = client.Close()
+	}()
 
 	time.Sleep(time.Second)
 
@@ -51,11 +54,19 @@ func main() {
 			defer wg.Done()
 			args := &Args{Num1: i, Num2: i * i}
 			var reply int
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
 				log.Fatalf("call Foo.Sum error: %v", err)
 			}
 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
+
+}
+
+func main() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go call(addr)
+	startServer(addr)
 }
